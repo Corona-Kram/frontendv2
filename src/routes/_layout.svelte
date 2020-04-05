10310d1b0076536438353fcdf6d2f552ab1dfd04
Jens Egholm <jensegholm@protonmail.com>
2020-04-05 18:18:12 +0200

    Typo

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -25,7 +25,7 @@
 		<p class="flow-text col s12 m8 offset-m2">
 			Med Coronakram.dk kan du sende et kram eller en besked til den person du synes trænger til en
 			kærlig hilsen.
-			Du kan også vælge at sætte dit kram eller din besked ind i en virtuel bank til en person du ikke kender.</p>
+			Du kan også vælge at sende dit kram eller din besked ind i en virtuel bank til en person du ikke kender.</p>
 	</div>
 </div>
 
